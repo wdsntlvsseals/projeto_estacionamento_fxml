f4e8188be523f521f33520a3b71534a800480160
Hugo Perlin <hugo.perlin@ifpr.edu.br>
2023-06-20 22:34:28 -0300

    incluindo a filtragem por placa

--- src/main/java/ifpr/pgua/eic/exemplo/models/Estacionamento.java
+++ src/main/java/ifpr/pgua/eic/exemplo/models/Estacionamento.java
@@ -2,6 +2,7 @@ package ifpr.pgua.eic.exemplo.models;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
@@ -15,6 +16,15 @@ public class Estacionamento {
         this.telefone = telefone;
         veiculos = new ArrayList<>();
     }
+
+    public void geraFakes(){
+        veiculos.add(new Veiculo("aaa-1121", "Modelo 1", "Fabricante 1", 2001, "Cor1"));
+        veiculos.add(new Veiculo("aab-1122", "Modelo 2", "Fabricante 2", 2002, "Cor2"));
+        veiculos.add(new Veiculo("aba-1123", "Modelo 3", "Fabricante 3", 2003, "Cor3"));
+        veiculos.add(new Veiculo("bbc-1124", "Modelo 4", "Fabricante 4", 2004, "Cor4"));
+        
+    }
+
 
     public String getNome() {
         return nome;
@@ -47,6 +57,11 @@ public class Estacionamento {
             }
         }
         return null;
+    }
+
+    /*Recebe o inicio de uma placa e retorna todos os veículos com aquele início de placa */
+    public List<Veiculo> filtrarVeiculosPlaca(String inicioPlaca){
+        return veiculos.stream().filter(veiculo -> veiculo.getPlaca().startsWith(inicioPlaca)).collect(Collectors.toList());
     }
 
     public boolean cadastraVeiculo(String placa, String modelo, String fabricante, int anoFabricacao, String cor){
